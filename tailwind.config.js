842df0074685e92ca6668c0e75823a2633d99cb8
unknown <onyenekwelizabeth@gmail.com>
2020-08-14 02:54:12 -0700

    configured tailwind css variables font-awesome

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        default: '#172b4d',
+        primary: '#5e72e4',
+        secondary: '#f4f5f7',
+        info: '#11cdef',
+        success: '#2dce89',
+        danger: '#f5365c',
+        warning: '#fb6340',
+        gray: {
+          100: '#f6f9fc',
+          200: '#e9ecef',
+          300: '#dee2e6',
+          400: '#ced4da',
+          500: '#adb5bd',
+          600: '#adb5bd',
+          700: '#525f7f',
+          800: '#32325d',
+          900: '#212529',
+        },
+      },
+    },
+  },
+}
